78baf27ec0f50035cb0619ab144a25f430356cf5
[Arpan Chaudhari] <arpanchaudhari810@gmail.com>
2026-07-16 08:35:47 +0530

    169. Majority Element

---
+++ Array/Medium/169MajorityElement.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def majorityElement(nums: List[int]) -> int:
+    candidate = 0
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+
+    return candidate
+
+nums = [3, 2, 3]
+print(majorityElement(nums))
