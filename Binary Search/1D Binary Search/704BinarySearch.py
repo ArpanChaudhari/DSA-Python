b4ae466cb839330355bdb824ad324e1e240a61a0
[Arpan Chaudhari] <arpanchaudhari810@gmail.com>
2026-07-29 20:30:34 +0530

    704. Binary Search

---
+++ Binary Search/1D Binary Search/704BinarySearch.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    n = len(nums)
+    low = 0
+    high = n - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(search(nums,target))
