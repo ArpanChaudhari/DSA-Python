e6ce74b452d1cae0edbe839fe4107491bf6f17d2
[Arpan Chaudhari] <arpanchaudhari810@gmail.com>
2026-08-10 22:47:47 +0530

    14. Longest Common Prefix

---
+++ String/14LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def longestCommonPrefix(strs: List[str]) -> str:
+    strs.sort()
+    first = strs[0]
+    last = strs[-1]
+    ans = ""
+
+    for i in range(min(len(first), len(last))):
+        if first[i] != last[i]:
+            return ans
+        ans += first[i]
+    return ans
+
+strs = ["flower","flow","flight"]
+print(longestCommonPrefix(strs))
